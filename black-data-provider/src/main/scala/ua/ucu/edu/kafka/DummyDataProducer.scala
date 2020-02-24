c75c7250a854dfbba2bd76bc710ee084e1d8f55a
akvilonBrown <akvilonBrown@gmail.com>
2020-02-24 02:40:00 +0200

    remodify black-data provider with environment variables

--- black-data-provider/src/main/scala/ua/ucu/edu/kafka/DummyDataProducer.scala
+++ black-data-provider/src/main/scala/ua/ucu/edu/kafka/DummyDataProducer.scala
@@ -12,6 +12,8 @@ import scala.io.Source
 object DummyDataProducer {
 
   val logger: Logger = LoggerFactory.getLogger(getClass)
+  val BLACKIPURL = "https://raw.githubusercontent.com/ihorhrysha/fraud-detection/master/black-data-provider/src/main/resources/black_ip.csv"
+  val BLACKEMAILURL = "https://raw.githubusercontent.com/ihorhrysha/fraud-detection/master/black-data-provider/src/main/resources/black_email.csv"
 
   def pushTestData(): Unit = {
     val BrokerList: String = System.getenv(Config.KafkaBrokers)
@@ -28,30 +30,33 @@ object DummyDataProducer {
 
 
     val producer = new KafkaProducer[String, String](props)
-
-    val testMsg = "i am black"
-
+    //val testMsg = "i am black"
     logger.info("reaching raw files")
+
+
     try {
+      println(Source.fromURL(BLACKIPURL).mkString.split(System.getProperty("line.separator")).toList)
+
+      val blackIpList : List[String] = Source.fromURL(BLACKIPURL).mkString.split(System.getProperty("line.separator")).toList
+      val blackEmailList : List[String] = Source.fromURL(BLACKEMAILURL).mkString.split(System.getProperty("line.separator")).toList
+
+
+
 
       while (true) {
-        Thread.sleep(1000)
-        logger.info(s"[$Topic] $testMsg")
-        val data = new ProducerRecord[String, String](Topic, testMsg)
-        producer.send(data, (metadata: RecordMetadata, exception: Exception) => {
-          logger.info(metadata.toString, exception)
-        })
+        blackIpList.foreach(ip => sendMessage(producer, Topic, ip))
+        blackEmailList.foreach(mail => sendMessage(producer, Topic, mail))
       }
 
     } catch {
       case e: FileNotFoundException => logger.error("File not found " + e)
-      case e: Exception => println(e)
+      case e: Exception =>println(e)
     }
 
     producer.close()
   }
 
-  def sendMessage(prod: KafkaProducer[String, String], topic: String, msg: String): Unit = {
+  def sendMessage(prod: KafkaProducer[String, String], topic: String, msg: String) : Unit =  {
     val precord = new ProducerRecord[String, String](topic, msg)
     prod.send(precord, (metadata: RecordMetadata, exception: Exception) => {
       logger.info(metadata.toString, exception)

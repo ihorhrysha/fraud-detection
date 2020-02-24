0ba622b8d2718076a7c51146a2a63f7c31531af9
Ihor Hrysha <i.hrysha@itprosteer.com>
2020-02-24 21:13:04 +0200

    conf changes

--- build_front.sh
+++ build_front.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -f ./front/.Dockerfile -t 768305773658.dkr.ecr.us-east-1.amazonaws.com/ucu-class/front:hrysha-0.1 ./front
+set -e  # exit immediately if a command exits with a non-zero status
+set -x  # print all executed commands on terminal
+
+export $(grep -v '^#' .env | xargs)
+
+docker build -f ./front/.Dockerfile -t 768305773658.dkr.ecr.us-east-1.amazonaws.com/ucu-class/front:$STUDENT_NAME-0.1 ./front
